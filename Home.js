488676c5a797334bf8d53647b040d04d12864222
Tanmay-Patel-21 <91056103+Tanmay-Patel-21@users.noreply.github.com>
2021-11-08 12:50:55 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -6,10 +6,11 @@ function expand()
 	var to = document.getElementById('to_select').value;
 
 	if(from!=to){
-	
-		document.getElementById('main_menu').style.height='260px';
+		document.getElementById('rem_body').style.height='1050px';
+		document.getElementById('main_menu').style.height='750px';
+		document.getElementById('chart_div').style.height='474px';
+		document.getElementById('chart_div').style.opacity='1';
 		document.getElementById('answer_container').style.display='flex';
-
 		document.getElementById('to_convert').innerHTML=(amount+" "+from);
 		document.getElementById('converted_answer').innerHTML=(amount+" "+to);
 		document.getElementById('conversion_rates').innerHTML=("1 "+from+" = w.xy "+to);
