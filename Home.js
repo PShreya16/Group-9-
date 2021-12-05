314a2286fd7a6c586e5aea849f60b213d2f4cc38
Tanmay-Patel-21 <91056103+Tanmay-Patel-21@users.noreply.github.com>
2021-12-05 11:35:12 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -4,6 +4,8 @@ function expand(){
 	document.getElementById('main_menu').style.height='750px';
 	document.getElementById('chart_div').style.height='474px';
 	document.getElementById('chart_div').style.opacity='1';
+	document.getElementById('myChart').style.height='474px';
+	document.getElementById('myChart').style.opacity='1';
 	flag=1;
 	calculate();
 }
@@ -18,14 +20,14 @@ function calculate() {
 	var to = document.getElementById('to_select').value;
 	var amount = document.getElementById('amount').value;
 	
-	fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=d2d2d6677c8b05a6b21eebf39b1744cc`)
+	fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=4953fc5871821ceed68ecd0dffb4e4b0`)
 	.then((res) => res.json())
 			.then((data) => {
 				const rate1 = data.rates[from];
 				const rate2 = data.rates[to];
 			    const converted_rate = rate2/rate1;
 				const final_amount = amount * converted_rate;       
-				document.getElementById('answer_container').style.height='100px';
+				document.getElementById('answer_container').style.height='110px';
 				document.getElementById('answer_container').style.opacity='1';
 				document.getElementById('to_convert').innerHTML=(amount+" "+from+" =");
 				document.getElementById('converted_answer').innerHTML=(final_amount+" "+to);
@@ -43,17 +45,21 @@ function calculate() {
 		var month = current_date.substring(5,7);
 		var day = current_date.substring(8,10);
 		var i = 0, j= 0;
-		for(i=0; i<7; i++){
+		for(i=0; i<5; i++){
+			// if(day.length<2){
+			// 	day="0"+day;
+			// }
+			// if(month.length<2){
+			// 	month="0"+month;
+			// }
 			var date = (year+"-"+month+"-"+day);
 			date_array[i]=date;
-			console.log(date);
-			fetch(`http://api.exchangeratesapi.io/v1/${date}?access_key=d2d2d6677c8b05a6b21eebf39b1744cc`)
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data.date+" "+data.rates.INR);
-			});
+			// fetch(`http://api.exchangeratesapi.io/v1/${date}?access_key=d2d2d6677c8b05a6b21eebf39b1744cc`)
+			// .then((res) => res.json())
+			// .then((data) => {
+			// 	console.log(data.date+" "+data.rates.INR);
+			// });
 		day = day-1;
-		
 		if(day<1){
 			month = month-1;
 			if(month<1){
@@ -71,42 +77,24 @@ function calculate() {
 			}
 		}
 	}	
-	console.log(date_array);
-	// for(i=0;i<7;i++){
-	// 		fetch(`http://api.exchangeratesapi.io/v1/${date_array[i]}?access_key=d2d2d6677c8b05a6b21eebf39b1744cc`)
-	// 		.then((res) => res.json())
-	// 			.then((data) => {
-	// 			console.log(data.date+" "+data.rates.INR);
 
-	// 		});
-	// }
-
-	const ctx = document.getElementById('myChart').getContext('2d');
-	const myChart = new Chart(ctx, {
-		type: 'line',
+	
+	past_rates=[123,123,1,3,13,1,313,13,12,23,1,3]
+		const ctx = document.getElementById('myChart').getContext('2d');
+		const myChart = new Chart(ctx, {
+				type: 'line',
 		data: {
-			labels: [date_array[6],date_array[5], date_array[4],date_array[3],date_array[2],date_array[1],date_array[0]],
+			labels: date_array,
 			datasets: [{
 				label: "Graph",
-				data: [1,6,3,21,5,-2,7],
+				data: past_rates,
 				fill: false,
-					
+				backgroindColor: 'rgba(0,0,0,1)',
 				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)' ,
-					'rgba(0,0,0,1)'
+					'rgba(255, 99, 132, 1)'
 				],
-				borderWidth: 3
+				borderWidth: 3,
+				lineTension: 0
 			}]
 		},
-		options: {
-			scales: {
-				y: {
-				}
-			}
-		}
 	});
